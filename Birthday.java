5fea64be51981972909328af63cc49945aaeb712
Charles Lynch <charlesloper2001@gmail.com>
2016-03-08 17:06:14 -0800

    Added files via upload

--- Birthday.java
+++ Birthday.java
@@ -5,19 +5,17 @@ public class Birthday
 
 	public static void main(String[] args) 
 	{
-		todaysDate();
-		yourBirthday();
-				
+		System.out.println(todaysDate().equals(yourBirthday()));
+		
 	}
 	
 	public static Date todaysDate()
 	{
-		System.out.print("What is today's date (month ex: 2 or 11 and day ex : 5 or 15)? ");
 		Scanner s = new Scanner(System.in);
+		System.out.print("What is today's date (month ex: 2 or 11 and day ex : 5 or 15)? ");
 		int month = s.nextInt();
 		int day = s.nextInt();
 		Date today = new Date (month, day);
-		s.close();
 		return today;
 	}
 	
--- Date.java
+++ Date.java
@@ -1,79 +1,84 @@
+//Charels Lynch
+//Date class
 
 public class Date 
 {
-	
-   private int day;
-   private int month;
-  
-   public Date(int month, int day) 
-   {       
-	   this.month = month;
-	   this.day = day;
-   }
-   
-   public int getMonth()
-   {
-	   return month;
-   }
-   
-   public int getDay()
-   {
-	   return day;
-   }
-   
-   public void setDate(int month, int day)
-   {
-	   this.month = month;
-	   this.day = day; 
-   }
-   
-   public String toString()
-   {
-	   return this.month + " / " + this.day;
-   }
-   
-   public boolean equals (Date d)
-   {
-	   if (this == d)
-	   {
-		   return true;
-	   }
-	   else
-	   {
-		   return false;
-	   }
-   }	   
 
-public int daysInMonth() 
-   {
-      int days;
-      switch(this.month) {
-         case 1:
-         case 3:
-         case 5:
-         case 7:
-         case 8:
-         case 10:
-         case 12:
-            days = 31;
-            break;
-         case 4:
-         case 6:
-         case 9:
-         case 11:
-            days = 30;
-            break;
-         case 2:
-            days = 28;
-            break;
-         default:
-            days = 0;
-      }
-      return days;
-   }
-   
-   public void nextDay() 
-   {
-      day++;
-   }
+	private int day;
+	private int month;
+
+	public Date(int month, int day) 
+	{       
+		this.month = month;
+		this.day = day;
+	}
+
+	public int getMonth()
+	{
+		return month;
+	}
+
+	public int getDay()
+	{
+		return day;
+	}
+
+	public void setDate(int month, int day)
+	{
+		this.month = month;
+		this.day = day; 
+	}
+
+	public String toString()
+	{
+		return this.month + " / " + this.day;
+	}
+	
+	public boolean equals(Date d) {
+		if (this == d)
+			return true;
+		if (d == null)
+			return false;
+		if (getClass() != d.getClass())
+			return false;
+		Date other = (Date) d;
+		if (day != other.day)
+			return false;
+		if (month != other.month)
+			return false;
+		return true;
+	}
+
+	public int daysInMonth() 
+	{
+		int days;
+		switch(this.month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			days = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = 30;
+			break;
+		case 2:
+			days = 28;
+			break;
+		default:
+			days = 0;
+		}
+		return days;
+	}
+
+	public void nextDay() 
+	{
+		day++;
+	}
 }
